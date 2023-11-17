a26efaabef6300e6fbcebbb41f6916d0284a0edd
Zboo-0324 <69633648+Zboo-0324@users.noreply.github.com>
2023-11-17 14:25:23 +0800

    Add files via upload

---
+++ autorun.py
@@ -0,0 +1,12 @@
+# -*- coding: gbk -*-
+
+
+import subprocess
+
+# subprocess.run(['python', 'CMFSL_IP_dixiyao_40.py'])
+# subprocess.run(['python', 'CMFSL_IP_dixiyao_50.py'])
+subprocess.run(['python', 'CMFSL_HU_10.py'])
+subprocess.run(['python', 'CMFSL_HU_20.py'])
+subprocess.run(['python', 'CMFSL_HU_30.py'])
+subprocess.run(['python', 'CMFSL_HU_40.py'])
+subprocess.run(['python', 'CMFSL_HU_50.py'])
